6560ec1c519ecc757cca761cc5207772892c7d87
slyboots <admin@slyb--ts.com>
2019-09-28 10:05:47 +0000

    initial commit

---
+++ __init__.py
@@ -0,0 +1,7 @@
+'''
+tilgui: Today I Learned GUI
+
+An app for creating / publishing posts for my [TIL blog](https://slyboots.github.io/til/) from my phone quickly and easily.
+
+_Written using the Pythonista app on my iPhone_
+
